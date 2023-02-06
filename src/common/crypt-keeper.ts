58710e5f1f77959b0719b463731d22f72f539f4e
Réza Kalfane <reza.kalfane@gmail.com>
2023-02-06 16:44:49 +0100

    chore: comments TODO

--- src/common/crypt-keeper.ts
+++ src/common/crypt-keeper.ts
@@ -17,10 +17,33 @@ const reverseString = str => str.split('').reverse().join('')
  */
 const CryptKeeper = (config: any, hub?: string) => {
 	const hash = `${reverseString(_.last(config._meta.deliveryId.split('-')))}${hub}${_.last(config._meta.schema.split('/'))}${reverseString(_.first(config._meta.deliveryId.split('-')))}`
+	
+	/**
+	 * TODO
+	 * @param text 
+	 * @returns 
+	 */
 	const encryptAES = (text: string): string => CryptoJS.AES.encrypt(text, hash).toString()
+
+	/**
+	 * TODO
+	 * @param text 
+	 * @returns 
+	 */
 	const decryptAES = (text: string): string => CryptoJS.AES.decrypt(text, hash).toString(CryptoJS.enc.Utf8)
 
+	/**
+	 * TODO
+	 * @param text 
+	 * @returns 
+	 */
 	const encrypt = (text: string): string => text.startsWith('===') && text.endsWith('===') ? text : `===${rot47(reverseString(encryptAES(text)))}===`
+
+	/**
+	 * TODO
+	 * @param text 
+	 * @returns 
+	 */
 	const decrypt = (text: string): string => text.startsWith('===') && text.endsWith('===') ? decryptAES(reverseString(rot47(text.substring(3, text.length - 3)))) : text
 
 	return {
