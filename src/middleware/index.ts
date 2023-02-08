36338e1d06950a91844f3a3d657d38360bbf16a1
Ray Sweeten <ray@raysweeten.com>
2023-02-08 10:07:50 -0500

    NOVADEV-1042 - adds support for codec_params in install params - backwards compat with flat params structure

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -5,8 +5,8 @@ import { isServer } from '../common/util'
 
 /**
  * TODO
- * @param config 
- * @returns 
+ * @param config
+ * @returns
  */
 const getAPI = async (config: any): Promise<CommerceAPI> => {
 	// we are passed in an object here
@@ -16,7 +16,7 @@ const getAPI = async (config: any): Promise<CommerceAPI> => {
 	//     - else retrieve the object with id <demostoreconfig.commerce.id>
 
 	if ('config_locator' in config) {
-		const [ hub, _ ] = config.config_locator.split(':')
+		const [hub, _] = config.config_locator.split(':')
 		config = await getContentItemFromConfigLocator(config.config_locator)
 		if (config._meta.schema === CONSTANTS.demostoreConfigUri) {
 			config = await getContentItem(hub, config.commerce)
@@ -24,7 +24,9 @@ const getAPI = async (config: any): Promise<CommerceAPI> => {
 	}
 
 	// novadev-582 Update SFCC codec to use client_id and client_secret to generate the api token if it doesn't exist
-	const matchingCodec = getCodecs().find(c => c.vendor === config.vendor || c.schemaUri === config._meta?.schema)
+	const matchingCodec = getCodecs().find(
+		(c) => c.vendor === config.vendor || c.schemaUri === config._meta?.schema
+	)
 	if (matchingCodec) {
 		config = await matchingCodec.postProcess(config)
 	}
@@ -36,23 +38,45 @@ const getAPI = async (config: any): Promise<CommerceAPI> => {
 /**
  * TODO
  */
-export type CommerceOperation = 'getProduct' | 'getProducts' | 'getCategory' | 'getMegaMenu' | 'getCustomerGroups' | 'getVariants' | 'getRawProducts'
+export type CommerceOperation =
+	| 'getProduct'
+	| 'getProducts'
+	| 'getCategory'
+	| 'getMegaMenu'
+	| 'getCustomerGroups'
+	| 'getVariants'
+	| 'getRawProducts'
 
 /**
  * TODO
- * @param params 
- * @returns 
+ * @param params
+ * @returns
  */
 // getCommerceAPI is the main client interaction point with the integration layer
 export const getCommerceAPI = async (params: any = undefined): Promise<CommerceAPI> => {
-	if (isServer()) {
-		return await getAPI(params)
-	}
-	else {
-		const getResponse = (operation: CommerceOperation) => async (args: any): Promise<any> => {
-			const apiUrl = (window as any).isStorybook ? 'https://core.dc-demostore.com/api' : '/api'
-			return await (await axios.get(apiUrl, { params: { ...args, ...params, operation } })).data
+	let codec = params
+
+	if (params.codec_params) {
+		codec = {
+			...params.codec_params,
+			vendor: params.vendor
 		}
+	}
+
+	//const codec = params.codec_params ?? params // merge in vendor with params
+	if (isServer()) {
+		return await getAPI(codec)
+	} else {
+		const getResponse =
+			(operation: CommerceOperation) =>
+			async (args: any): Promise<any> => {
+				const apiUrl = (window as any).isStorybook
+					? 'https://core.dc-demostore.com/api'
+					: '/api'
+				return await (
+					await axios.get(apiUrl, { params: { ...args, ...codec, operation } })
+				).data
+			}
 
 		return {
 			getProduct: getResponse('getProduct'),
@@ -62,15 +86,16 @@ export const getCommerceAPI = async (params: any = undefined): Promise<CommerceA
 			getCustomerGroups: getResponse('getCustomerGroups'),
 			getVariants: getResponse('getVariants'),
 			getRawProducts: getResponse('getRawProducts')
+			//getPromotions: getResponse('getPromotions')
 		}
 	}
 }
 
 /**
  * TODO
- * @param req 
- * @param res 
- * @returns 
+ * @param req
+ * @param res
+ * @returns
  */
 // handler for /api route
 export const middleware = async (req, res) => {
@@ -82,15 +107,15 @@ export const middleware = async (req, res) => {
 	const config = req.body || req.query
 	const commerceAPI = await getCommerceAPI(config)
 	switch (req.method.toLowerCase()) {
-	case 'get':
-	case 'post':
-		return res.status(200).json(await commerceAPI[config.operation](config))
+		case 'get':
+		case 'post':
+			return res.status(200).json(await commerceAPI[config.operation](config))
 
-	case 'options':
-		return res.status(200).send()
+		case 'options':
+			return res.status(200).send()
 
-	default:
-		break
+		default:
+			break
 	}
 }
 
