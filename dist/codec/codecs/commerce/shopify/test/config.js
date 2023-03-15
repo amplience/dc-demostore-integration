70bbf592c63fe4ba51a6afa7987fb1f4132ba214
Réza Kalfane <reza.kalfane@gmail.com>
2023-03-15 17:47:43 +0100

    chore: added config

---
+++ dist/codec/codecs/commerce/shopify/test/config.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.config = void 0;
+exports.config = {
+    vendor: 'shopify',
+    codec_params: {
+        access_token: 'access_token',
+        version: 'version',
+        site_id: 'site_id'
+    }
+};
