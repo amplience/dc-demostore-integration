e234e37fcb0ad37fdb8fc3d24d6463d05e8a86fb
Réza Kalfane <reza.kalfane@gmail.com>
2023-03-15 10:37:37 +0100

    chore: shopify base template

---
+++ src/codec/codecs/commerce/shopify/index.ts
@@ -0,0 +1,124 @@
+import { 
+	APIProperties,
+	ClientCredentialProperties,
+	ClientCredentialsConfiguration,
+	CommerceAPI, 
+	CommonArgs, 
+	GetProductsArgs, 
+	Identifiable, 
+	Product 
+} from '../../../../common'
+import _ from 'lodash'
+import { CodecPropertyConfig, CommerceCodecType, CommerceCodec } from '../../core'
+import { getProductsArgError } from '../../common'
+import { StringProperty } from '@/codec/cms-property-types'
+
+/**
+ * Common codec configuration.
+ */
+type CodecConfig = ClientCredentialsConfiguration
+
+/**
+ * A template commerce codec type, useful as a starting point for a new integration.
+ */
+export class TemplateCommerceCodecType extends CommerceCodecType {
+
+	/**
+	 * @inheritdoc
+	 */
+	get vendor(): string {
+		return 'template'
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	get properties(): CodecConfig {
+		return {
+			...ClientCredentialProperties
+		}
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	async getApi(config: CodecPropertyConfig<CodecConfig>): Promise<CommerceAPI> {
+		return await new TemplateCommerceCodec(config).init(this)
+	}
+}
+
+/**
+ * A template commerce codec, useful as a starting point for a new integration.
+ */
+export class TemplateCommerceCodec extends CommerceCodec {
+	declare config: CodecPropertyConfig<CodecConfig>
+
+	// instance variables
+	// products: Product[]
+	// categories: Category[]
+
+	/**
+	 * @inheritdoc
+	 */
+	async init(codecType: CommerceCodecType): Promise<CommerceCodec> {
+		// this.products = await fetchFromURL(this.config.productURL, [])
+		// this.megaMenu = this.categories.filter(cat => !cat.parent)
+		return await super.init(codecType)
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	async getProducts(args: GetProductsArgs): Promise<Product[]> {
+		// eslint-disable-next-line no-empty
+		if (args.productIds) {
+
+		}
+		// eslint-disable-next-line no-empty
+		else if (args.keyword) {
+
+		}
+		// eslint-disable-next-line no-empty
+		else if (args.category) {
+
+		}
+		else {
+			throw getProductsArgError('getProducts')
+		}
+
+		return []
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	async getRawProducts(args: GetProductsArgs): Promise<any[]> {
+		// eslint-disable-next-line no-empty
+		if (args.productIds) {
+
+		}
+		// eslint-disable-next-line no-empty
+		else if (args.keyword) {
+
+		}
+		// eslint-disable-next-line no-empty
+		else if (args.category) {
+			
+		}
+		else {
+			throw getProductsArgError('getRawProducts')
+		}
+
+		return []
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	async getCustomerGroups(args: CommonArgs): Promise<Identifiable[]> {
+		return []
+	}
+}
+
+export default TemplateCommerceCodecType
+// registerCodec(new TemplateCommerceCodecType())
