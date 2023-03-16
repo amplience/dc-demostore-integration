e71fbad01a68d54c0728b71e04760646f33fecaa
Réza Kalfane <reza.kalfane@gmail.com>
2023-03-16 11:07:46 +0100

    chore: format

--- dist/codec/codecs/commerce/shopify/queries.js
+++ dist/codec/codecs/commerce/shopify/queries.js
@@ -1,91 +1,103 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.productsByCategory = exports.productById = exports.productsByQuery = exports.productShared = void 0;
+exports.segments = exports.productsByCategory = exports.productById = exports.productsByQuery = exports.productShared = void 0;
 exports.productShared = `
 id
 title
 description
 collections(first: 100) {
-  edges {
-	node {
-	  id
-	  handle
-	  title
+	edges {
+		node {
+			id
+			handle
+			title
+		}
+		cursor
 	}
-	cursor
-  }
 }
 tags
 variants(first: 100) {
-  edges {
-	node {
-	  id
-	  title
-	  sku
-	  selectedOptions {
-		name
-		value
-	  }
-	  price {
-		currencyCode
-		amount
-	  }
-	  unitPrice {
-		currencyCode
-		amount
-	  },
-	  compareAtPrice {
-		currencyCode
-		amount
-	  }
-	  image {
-		id
-		url
-		altText
-	  }
+	edges {
+		node {
+			id
+			title
+			sku
+			selectedOptions {
+				name
+				value
+			}
+			price {
+				currencyCode
+				amount
+			}
+			unitPrice {
+				currencyCode
+				amount
+			}
+			compareAtPrice {
+				currencyCode
+				amount
+			}
+			image {
+				id
+				url
+				altText
+			}
+		}
+		cursor
 	}
-	cursor
-  }
 }
 images(first: 100) {
-  edges {
-	node {
-	  id
-	  url
-	  altText
+	edges {
+		node {
+			id
+			url
+			altText
+		}
+		cursor
 	}
-	cursor
-  }
 }
 availableForSale
 handle`;
 exports.productsByQuery = `
 query getProducts($pageSize: Int!, $query: String, $after: String){
-  products(first: $pageSize, after: $after, query: $query) {
-    edges {
-      node {
-${exports.productShared}
-      }
-      cursor
-    }
-  }
+	products(first: $pageSize, after: $after, query: $query) {
+		edges {
+			node {
+				${exports.productShared}
+			}
+			cursor
+		}
+	}
 }`;
 exports.productById = `
 query getProductById($id: ID!) {
 	product(id: $id) {
-${exports.productShared}
+		${exports.productShared}
 	}
 }`;
 exports.productsByCategory = `
 query getProductsByCategory($handle: String!, $pageSize: Int!, $after: String) {
-  collection(handle: $handle) {
-    products(first: $pageSize, after: $after) {
-      edges {
-        node {
-${exports.productShared}
-        }
-        cursor
-      }
-    }
-  }
+	collection(handle: $handle) {
+		products(first: $pageSize, after: $after) {
+			edges {
+				node {
+					${exports.productShared}
+				}
+				cursor
+			}
+		}
+	}
+}`;
+exports.segments = `
+query getSegments($pageSize: Int!, $after: String) {
+	segments(first: $pageSize, after: $after) {
+	  	edges {
+			node {
+				id
+				name
+				query
+			}
+	  	}
+	}
 }`;
